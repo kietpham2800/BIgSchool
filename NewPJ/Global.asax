15ff66b00bc5838832cf59f8a696170c98228561
kietpham2800 <kietpham2800@gmail.com>
2021-06-28 09:28:51 +0700

    Add project files.

---
+++ NewPJ/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewPJ.MvcApplication" Language="C#" %>
---
+++ NewPJ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewPJ.Startup))]
+namespace NewPJ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
